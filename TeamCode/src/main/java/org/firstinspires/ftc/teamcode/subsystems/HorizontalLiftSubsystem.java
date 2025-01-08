aaa5c7feeb8a6b3c8b79edaf8151d79f69eadf61
Lucas Jiao <lucas.jiao.ny@gmail.com>
2025-01-08 16:06:01 -0500

    push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HorizontalLiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HorizontalLiftSubsystem.java
@@ -7,28 +7,50 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class HorizontalLiftSubsystem extends SubsystemBase {
-    public final MotorEx motor;
+    public final MotorEx horizontalLift;
     private final PIDController controller;
     private MotionProfile profile;
     public MotionState state;
-    public static double P = 0.17;
-    public static double I = 0;
-    public static double D = 0;
-    public double currentLiftPosition;
-    public double targetLiftPosition;
+
+    public static double P = 0.17, I = 0.0, D = 0.0;
+    private final ElapsedTime timer, voltageTimer;
+    private double liftPosition, targetLiftPosition;
+
     public double liftPower;
-    private final double slidesTickPerInch = 2 * Math.PI * 0.701771654 / 145.1;
+    private final double SLIDE_TICK = 2 * Math.PI * 0.701771654 / 145.1;
     private boolean isAuto = false;
     public HorizontalLiftSubsystem(HardwareMap hardwareMap, boolean isAuto){
-        motor = new MotorEx(hardwareMap, "horizontalMotor");
+        horizontalLift = new MotorEx(hardwareMap, "horizontalMotor");
         controller = new PIDController(P, I, D);
         controller.setPID(P, I, D);
         this.isAuto = isAuto;
 
+
         profile = MotionProfileGenerator.generateSimpleMotionProfile(new MotionState(1, 0),
                 new MotionState(0,0), 0, 0);
         // values are not final
+        timer = new ElapsedTime(); voltageTimer = new ElapsedTime();
     }
+
+    public void read(){
+        liftPosition = horizontalLift.encoder.getPosition() * SLIDE_TICK;
+    }
+    public void write(){
+        horizontalLift.set(liftPower);
+    }
+
+    public void setTargetLiftPosition(double v){targetLiftPosition = v;}
+
+    public double getLiftPosition(){return liftPosition;}
+
+    public void newProfile(double targetPos, double max_v, double max_a){
+        profile = MotionProfileGenerator.generateSimpleMotionProfile(
+                new MotionState(liftPosition, 0),
+                new MotionState(targetPos, 0), max_v, max_a
+        );
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VerticalLiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/VerticalLiftSubsystem.java
@@ -1,44 +1,59 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.acmerobotics.roadrunner.profile.MotionProfile;
+import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
 import com.acmerobotics.roadrunner.profile.MotionState;
+import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class VerticalLiftSubsystem {
+public class VerticalLiftSubsystem extends SubsystemBase {
     public final MotorEx lift1, lift2;
+    private final PIDController controller;
     private MotionProfile profile;
-    public MotionState currentState;
+    public MotionState state;
 
+    public static double P = 0.17, I = 0.0, D = 0.0;
     private final ElapsedTime timer, voltageTimer;
-    private final PIDController liftController;
-    private final VoltageSensor voltageSensor;
+    private double liftPosition, targetLiftPosition;
 
-
-    private double voltage;
-    private double liftPosition;
-
-
-    public static double P = 0.26;
-    public static  double I = 0;
-    public static double D = 0;
-    public static double kG = 0.21;
-
-    private final double SLIDE_TICKS_PER_INCH = 2 * Math.PI * 0.764445002 / 145.1;
-
+    public double liftPower;
+    private final double SLIDE_TICK = 2 * Math.PI * 0.701771654 / 145.1;
     private boolean isAuto = false;
-
-    public double liftPower = 0.0;
-    public double liftTargetPosition = 0.0;
     public VerticalLiftSubsystem(HardwareMap hardwareMap, boolean isAuto){
-        lift1 = new MotorEx(hardwareMap, "verticalMotor1");
-        lift2 = new MotorEx(hardwareMap, "verticalMotor2");
+        lift1 = new MotorEx(hardwareMap, "verticalM1");
+        lift2 = new MotorEx(hardwareMap, "verticalM2");
+        controller = new PIDController(P, I, D);
+        controller.setPID(P, I, D);
+        this.isAuto = isAuto;
 
-        lift2.setInverted(true);
-        lift2.encoder.setDirection(MotorEx.Direction.REVERSE);
 
+        profile = MotionProfileGenerator.generateSimpleMotionProfile(new MotionState(1, 0),
+                new MotionState(0,0), 0, 0);
+        // values are not final
+        timer = new ElapsedTime(); voltageTimer = new ElapsedTime();
     }
+
+    public void read(){
+        liftPosition = lift1.encoder.getPosition() * SLIDE_TICK;
+        liftPosition = lift2.encoder.getPosition() * SLIDE_TICK;
+    }
+    public void write(){
+        lift1.set(liftPower);
+        lift2.set(-liftPower);
+    }
+
+    public void setTargetLiftPosition(double v){targetLiftPosition = v;}
+
+    public double getLiftPosition(){return liftPosition;}
+
+    public void newProfile(double targetPos, double max_v, double max_a){
+        profile = MotionProfileGenerator.generateSimpleMotionProfile(
+                new MotionState(liftPosition, 0),
+                new MotionState(targetPos, 0), max_v, max_a
+        );
+    }
+
 }
