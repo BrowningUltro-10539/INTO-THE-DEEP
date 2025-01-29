52de3d1c14cd30a335579c393b392216ede19292
Yichen Lucas Jiao <lucas.jiao.ny@gmail.com>
2025-01-29 14:35:16 -0500

    Auto Experimentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OuttakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OuttakeSubsystem.java
@@ -6,12 +6,11 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
 public class OuttakeSubsystem {
     public ServoImplEx leftArm, rightArm, claw, rotate;
     public static double ARM_RIGHT = 0;
-    public static double ARM_PICKUP = 0.8; // position of outtake arm that picks up from intake claw
-    public static double ARM_MIDPOINT = 0.4;
+    public static double ARM_PICKUP_FROM_INTAKE = 0.8; // position of outtake arm that picks up from intake claw
+    public static double ARM_MIDPOINT = 0.4; // max height of arm
 
-    public static double ARM_DEPOSIT = 0.2;// max height
-
-
+    public static double ARM_DEPOSIT = 0.2; // arm position when we deposit samples and specimen.
+    public static double ARM_PICKUP_SPECIMEN = 0.9;
     public static double CLAW_OPEN = 0.5, CLAW_CLOSE = 0.32;
 
     public static double ROTATE_TRANSFER = 0.6; // claw rotate position to transfer from intake -> outtake
